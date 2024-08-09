928e34c06c8958a16a5380d0c75a684d4ee7db44
digiflipmark <62587287+digiflipmark@users.noreply.github.com>
2024-08-09 12:35:51 +0530

    Revert "working on pagingSource"

--- app/src/main/java/com/finder/movieapp/core_feature/presentation/home_screen/HomeScreen.kt
+++ app/src/main/java/com/finder/movieapp/core_feature/presentation/home_screen/HomeScreen.kt
@@ -1,139 +1,54 @@
 package com.finder.movieapp.core_feature.presentation.home_screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
+import android.annotation.SuppressLint
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.R
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.compose.LazyPagingItems
-import androidx.paging.compose.collectAsLazyPagingItems
-import com.finder.movieapp.R
-import com.finder.movieapp.core_feature.data.remote.dto.Result
-import com.finder.movieapp.core_feature.domain.util.MoviesGenre
 import com.finder.movieapp.core_feature.presentation.comman.SearchBar
-import com.finder.movieapp.core_feature.presentation.home_screen.componet.MovieCard
-import com.finder.movieapp.core_feature.presentation.home_screen.componet.TrendingMovieCard
-import com.finder.movieapp.core_feature.presentation.util.componets.MoviesTabLayout
-import com.finder.movieapp.core_feature.presentation.util.componets.gridItems
+import com.finder.movieapp.core_feature.presentation.search_screen.SearchScreen
+import com.finder.movieapp.ui.theme.MovieAppTheme
 
 @Composable
-fun HomeScreen(homeViewModel: HomeViewModel = hiltViewModel()) {
+fun HomeScreen() {
 
     val state = rememberLazyListState()
-    val trendingMovies = homeViewModel.trendingMovies.collectAsLazyPagingItems()
-    val multipleMovies = homeViewModel.pageFlow.collectAsLazyPagingItems()
-    val selectedIndex = homeViewModel.tabLayoutMoviesState.collectAsState().value
-    val movieCategory = rememberMoviesCategories()
 
     LazyColumn(modifier = Modifier.fillMaxSize(), state = state) {
 
         item {
-
             Spacer(modifier = Modifier.height(10.dp))
 
             HeaderText(
-                modifier = Modifier.padding(horizontal = 24.dp), text = "What do you want to watch?"
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "What do you want to watch?"
             )
 
             Spacer(modifier = Modifier.height(24.dp))
 
-            SearchBar(modifier = Modifier
-                .padding(horizontal = 24.dp)
-                .fillMaxWidth()
-                .height(50.dp),
+            SearchBar(
+                modifier = Modifier
+                    .padding(horizontal = 24.dp)
+                    .fillMaxWidth()
+                    .height(42.dp),
                 text = "",
-                onTextChange = {},
-                readonly = true,
-                onClick = {})
+                onTextChange = {}, readonly = true, onClick = {}
+            )
 
             Spacer(modifier = Modifier.height(20.dp))
-
-            TrendingMovieSection(trending = trendingMovies, onClick = {
-
-            })
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            MoviesTabLayout(movieCategory,
-                selectedIndex = selectedIndex.selectedIndex,
-                onTabSelect = {
-                    homeViewModel.changeMoviesTab(it.tab)
-                })
-            Spacer(modifier = Modifier.height(20.dp))
-        }
-
-
-        gridItems(
-            multipleMovies,
-            3,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 24.dp),
-            verticalSpace = 18.dp,
-            horizontalArrangement = Arrangement.SpaceAround,
-        ) {
-
-            it?.let {
-                MovieCard(
-                    movieItem = it, modifier = Modifier
-                        .width(100.dp)
-                        .height(146.dp)
-                        .padding(horizontal = 11.dp)
-                ) {
-
-                }
-            }
-
-
-        }
-
-    }
-
-}
-
-
-@Composable
-fun TrendingMovieSection(trending: LazyPagingItems<Result>, onClick: (Result) -> Unit) {
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(250.dp)
-    ) {
-
-        LazyRow(
-            horizontalArrangement = Arrangement.spacedBy(20.dp),
-            contentPadding = PaddingValues(start = 24.dp),
-            modifier = Modifier.fillMaxSize()
-        ) {
-
-            items(trending.itemCount) {
-
-                trending[it]?.let { result ->
-
-                    TrendingMovieCard(
-                        movieItem = result,
-                        onClick = { onClick(result) },
-                        number = it + 1
-                    )
-                }
-
-            }
 
 
         }
@@ -146,24 +61,27 @@ fun HeaderText(
     text: String,
 ) {
     Text(
-        text = text, style = MaterialTheme.typography.headlineLarge, modifier = modifier
+        text = text,
+        style = MaterialTheme.typography.headlineLarge,
+        modifier = modifier
     )
 }
 
 
+@Preview
 @Composable
-fun rememberMoviesCategories(): List<MovieTab> {
+fun HeaderTextPreview() {
+    MovieAppTheme {
 
-    return listOf(
-        MovieTab(
-            title = stringResource(id = R.string.now_playing), tab = MoviesGenre.NOW_PLAYING
-        ), MovieTab(
-            title = stringResource(id = R.string.upcoming), tab = MoviesGenre.UPCOMING
-        ), MovieTab(
-            title = stringResource(id = R.string.top_rated), tab = MoviesGenre.TOP_RATED
-        ), MovieTab(
-            title = stringResource(id = R.string.popular), tab = MoviesGenre.POPULAR
-        )
-    )
+        HeaderText(modifier = Modifier, text = "What do you want to watch?")
+    }
 }
 
+@Preview
+@Composable
+private fun DisplayHome() {
+
+    MovieAppTheme {
+        HomeScreen()
+    }
+}
--- app/src/main/java/com/finder/movieapp/core_feature/presentation/home_screen/MovieTab.kt
+++
@@ -1,5 +0,0 @@
-package com.finder.movieapp.core_feature.presentation.home_screen
-
-import com.finder.movieapp.core_feature.domain.util.MoviesGenre
-
-data class MovieTab(val title: String, val tab: MoviesGenre)
